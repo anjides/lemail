0e53b8b2562b6f92858f0a3ce82b1e612c5d4e89
Eugene Yaroslavtsev <anjides@gmail.com>
2014-09-16 15:31:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## Le Mail
+Write e-mail faster.
+
+#### Project motivations
+I don't like creating things that don't provide a "[quantum of utility](https://news.ycombinator.com/item?id=542768)". So I thought that, even with this project, I'd turn it into something that I'd actually use. 
+
+E-mail should not take as much time as it does for most people right now. Having observed those who I consider good at e-mail, one thing I've noticed is how quickly they write and send them out. With this in mind, I created two rules for sending e-mail faster:
+
+- the email must be written in less than 5 minutes
+- you must type something at least every 10 seconds
+
+If you don't do follow these two rules, the e-mail gets erased. These two rules act a forcing function to let you get thru e-mails faster and get back to doing whatever it is you'd rather be doing instead.
+
+#### My background
+My experience is a mix of:
+
+- node.js (backend)
+- backbone, angular (frontend)
+
+I've definitely spent more time on the backend than on frontend, so please take note of that when looking at the frontend code.
+
+
+#### Components
+
+##### API backend
+
+Tech used:
+
+- node.js
+- express (routing, parsing, validation)
+- superagent (sending HTTP requests)
+- lodash (general utilities)
+
+##### Web client
+
+Tech used:
+
+- backbone.js
+- Facebook Flux
+- Facebook React
---
+++ config.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2014 Eugene Yaroslavtsev
+
+// Permission to use, copy, modify, and/or distribute this software for any purpose
+// with or without fee is hereby granted, provided that the above copyright notice
+// and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+'use strict';
+
+var _ = require('lodash');
+
+var config = {
+	
+	development: {
+		appName: 		process.env.APP_NAME || 'lemail-api-dev',
+		port: 			process.env.PORT || 8000,
+		logLevel: 		process.env.LOG_LEVEL || 'debug'
+	},
+
+	production: {
+		appName: 		process.env.APP_NAME || 'lemail-api',
+		port: 			process.env.PORT || 8000,
+		logLevel: 		process.env.LOG_LEVEL || 'error'
+	}
+
+};
+
+var env = process.env.NODE_ENV;
+
+if (!env) {
+	console.warn('NODE_ENV environment variable not set, defaulting to development');
+	env = 'development';
+}
+
+module.exports = _.extend({}, config[env], { env: env });
